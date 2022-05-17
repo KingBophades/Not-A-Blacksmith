0ca3cd9096ef55699213cd18e3a7e0b3423cc6b0
NoScopingJFK <30759644+NoScopingJFK@users.noreply.github.com>
2022-05-17 04:16:14 -0700

    My first 2 modded items

---
+++ src/main/java/net/bophades/nab/item/ModItems.java
@@ -0,0 +1,27 @@
+package net.bophades.nab.item;
+
+import net.bophades.nab.NotABlacksmith;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class ModItems {
+
+    public static final Item MYTHRIL_INGOT = registerItem("mythril_ingot",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    public static final Item MYTHRIL_NUGGET = registerItem("mythril_nugget",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(NotABlacksmith.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        NotABlacksmith.LOGGER.info("Registering Mod Items for " + NotABlacksmith.MOD_ID);
+    }
+
+}
