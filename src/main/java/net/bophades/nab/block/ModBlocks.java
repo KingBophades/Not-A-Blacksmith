97d66718e4f57dd2ece26dd3ee08e245c9bf118e
NoScopingJFK <30759644+NoScopingJFK@users.noreply.github.com>
2022-05-18 11:26:18 -0700

    Finished adding in first block (Mythril Block)

--- src/main/java/net/bophades/nab/block/ModBlocks.java
+++ src/main/java/net/bophades/nab/block/ModBlocks.java
@@ -1,8 +1,11 @@
 package net.bophades.nab.block;
 
+import jdk.dynalink.NamedOperation;
 import net.bophades.nab.NotABlacksmith;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
+import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -10,11 +13,17 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModBlocks {
-//get back to in a bit
+
+    public static final Block MYTHRIL_BLOCK = registerBlock("mythril_block",
+            new Block(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()), ItemGroup.MISC);
+
+
+
     private static Block registerBlock(String name, Block block, ItemGroup group) {
         registerBlockItem(name, block, group);
+        return Registry.register(Registry.BLOCK, new Identifier(NotABlacksmith.MOD_ID,name), block);
     }
-
+//basically registers block item
     private static Item registerBlockItem(String name, Block block, ItemGroup group) {
         return Registry.register(Registry.ITEM, new Identifier(NotABlacksmith.MOD_ID, name),
                 new BlockItem(block, new FabricItemSettings().group(group)));
